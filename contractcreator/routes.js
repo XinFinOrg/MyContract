f9d34d0d72441c99a18b7894d93226d5b9f0ea23
akshaypilankar <pilankar.akshay3@gmail.com>
2018-08-16 14:26:59 +0530

    custom and recommended pages added

--- contractcreator/routes.js
+++ contractcreator/routes.js
@@ -1,3 +1,12 @@
 module.exports = function(app){
+
+
+    app.get('/customContract', function(req, res) {
+        res.render('customContract');
+      });
+
+      app.get('/recommendedContract', function(req, res) {
+        res.render('recommendedContract');
+      });
   
 }
