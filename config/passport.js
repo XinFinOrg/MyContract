c1086f9182e3e565354a419729b7baab48197b58
nishant1307 <nishant.mitra94@gmail.com>
2018-08-16 13:51:18 +0530

    Githb login + Contract Creator initiated

--- config/passport.js
+++ config/passport.js
@@ -17,7 +17,6 @@ module.exports = function(passport) {
   // =========================================================================
   // required for persistent login sessions
   // passport needs ability to serialize and unserialize users out of session
-
   // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
     done(null, user.id);
