00d194e569e357dc95659b7df4327cfd1b790ba8
nishant1307 <nishant.mitra94@gmail.com>
2018-10-02 16:39:48 +0530

    UI changes

---
+++ icoDashboardCreator/dist/assets/js/custom.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+   
+<!-- Copy to clipboard -->
+(function() {
+	'use strict';  
+  // click events
+  document.body.addEventListener('click', copy, true);
+	// event handler
+	function copy(e) {
+    // find target element
+    var 
+      t = e.target,
+      c = t.dataset.copytarget,
+      inp = (c ? document.querySelector(c) : null);      
+    // is element selectable?
+    if (inp && inp.select) {      
+      // select text
+      inp.select();
+      try {
+        // copy text
+        document.execCommand('copy');
+        inp.blur();        
+        // copied animation
+        t.classList.add('copied');
+        setTimeout(function() { t.classList.remove('copied'); }, 1000);
+      }
+      catch (err) {
+        alert('please press Ctrl/Cmd+C to copy');
+      }      
+    }    
+	}
+})();
