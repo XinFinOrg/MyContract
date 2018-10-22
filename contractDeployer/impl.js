fd0161f65d21bfa3fb092c10b46e15d60bad126d
akshaypilankar <pilankar.akshay3@gmail.com>
2018-10-22 17:21:15 +0530

    hf

--- contractDeployer/impl.js
+++ contractDeployer/impl.js
@@ -8,8 +8,13 @@ var ProjectConfiguration = db.projectConfiguration;
 var userCurrencyAddress = db.userCurrencyAddress;
 var ejs = require("ejs");
 var fileReader = require('../filereader/impl');
+var config = require('../config/paymentListener');
 var Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
+var ws_provider = config.ws_provider;
+var provider = new Web3.providers.WebsocketProvider(ws_provider);
+var web3 = new Web3(provider);
+
 module.exports = {
   getBytecode: async function (req, res) {
     var coinName = req.query.coinName;
@@ -191,34 +196,34 @@ module.exports = {
     //console.log(req.query) // network: 'test', coinName: 'pilankarcoin' 
     let projectData = await ProjectConfiguration.find({ where: { 'coinName': 'pilankarcoin' } });
     let accountData = await userCurrencyAddress.find({ where: { 'client_id': '9cdeae50-d458-11e8-a2ce-5d8d68895880', 'currencyType': 'Ethereum' } })
-    console.log(projectData.dataValues,accountData.dataValues)
+    console.log(projectData.dataValues, accountData.dataValues)
     byteCode = await solc.compile(projectData.tokenContractCode, 1).contracts[':Coin']
     projectData.tokenByteCode = byteCode.bytecode;
     projectData.tokenABICode = byteCode.interface;
-    byteCode = byteCode.bytecode;
-    let provider = new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws');
-    let web3 = new Web3(provider);
-    var privateKey = new Buffer('25F8170BA33240C0BD2C8720FE09855ADA9D07E38904FC5B6AEDCED71C0A3142', 'hex')
-    let txData = {
-      "nonce": await web3.eth.getTransactionCount('0x14649976AEB09419343A54ea130b6a21Ec337772'),
-      "gasPrice": "0x170cdc1e00",
-      "gasLimit": "0x2dc6c0",
-      "to": "",
-      "value": "0x00",
-      "data": req.body.data,
-      "chainId": 3
-    }
-    var tx = new Tx(txData);
-    tx.sign(privateKey);
-    var serializedTx = tx.serialize();
-    console.log("in here")
-    web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-      .on('receipt', async function (receipt) {
-        projectData.tokenContractAddress = receipt.contractAddress;
-        projectData.tokenContractHash = receipt.transactionHash;
-        await projectData.save();
-        res.send(receipt);
-      });
+    res.send(projectData.tokenByteCode);
+  //   let provider = new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws');
+  //   let web3 = new Web3(provider);
+  //   var privateKey = new Buffer('25F8170BA33240C0BD2C8720FE09855ADA9D07E38904FC5B6AEDCED71C0A3142', 'hex')
+  //   let txData = {
+  //     "nonce": await web3.eth.getTransactionCount('0x14649976AEB09419343A54ea130b6a21Ec337772'),
+  //     "gasPrice": "0x170cdc1e00",
+  //     "gasLimit": "0x2dc6c0",
+  //     "to": "",
+  //     "value": "0x00",
+  //     "data": byteCode.bytecode,
+  //     "chainId": 3
+  //   }
+  //   var tx = new Tx(txData);
+  //   tx.sign(privateKey);
+  //   var serializedTx = tx.serialize();
+  //   console.log("in here")
+  //   web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+  //     .on('receipt', async function (receipt) {
+  //       projectData.tokenContractAddress = receipt.contractAddress;
+  //       projectData.tokenContractHash = receipt.transactionHash;
+  //       await projectData.save();
+  //       res.send(receipt);
+  //     });
   },
 };
 
