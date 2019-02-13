0f69fdfea2689f3c30530989e72fbee8bdc6fdf2
akshaypilankar <pilankar.akshay3@gmail.com>
2019-02-13 19:06:04 +0530

    faqs and api doc link added

--- routes.js
+++ routes.js
@@ -1,16 +1,19 @@
-module.exports = function(app){
-  app.get('/', isLoggedIn, function(req, res) {
+module.exports = function (app) {
+  app.get('/', isLoggedIn, function (req, res) {
 
     res.render('landingPage');
   });
-  app.get('/privacyPolicy', function(req, res) {
+  app.get('/privacyPolicy', function (req, res) {
 
     res.render('privacyPolicy');
   });
-  app.get('/termsConditions', function(req, res) {
+  app.get('/termsConditions', function (req, res) {
 
     res.render('termsConditions');
   });
+  app.get('/faqs', function (req, res) {
+    res.render('faqs')
+  })
 };
 
 // route middleware to make sure a user is logged in
