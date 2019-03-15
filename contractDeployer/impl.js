5771ef08fa3f6d5ec539589f9952a02b2691fce3
Rahulraj04 <rahulrajnair94@gmail.com>
2019-03-15 16:09:25 +0530

    ContractDeployer methode separated

--- contractDeployer/impl.js
+++ contractDeployer/impl.js
@@ -9,6 +9,7 @@ var ProjectConfiguration = db.projectConfiguration;
 var userCurrencyAddress = db.userCurrencyAddress;
 var ejs = require("ejs");
 var fileReader = require('../filereader/impl');
+var contractDeployerListener = require('./contractDeployerListener');
 var config = require('../config/paymentListener');
 var privateICOhandler = require('../icoHandler/privateNetworkHandler')
 var etherRopstenICOhandler = require('../icoHandler/etherRopstenNetworkHandler')
@@ -67,49 +68,8 @@ module.exports = {
         try {
           privateICOhandler.sendEther(accountData.address, '0x06f05b59d3b20000')
             .then(async r => {
-              
-              byteCode = await solc.compile(projectData.tokenContractCode, 1).contracts[':Coin']
-              projectData.tokenByteCode = byteCode.bytecode;
-              projectData.tokenABICode = byteCode.interface;
-              privateICOhandler.sendTransaction(accountData.address, byteCode.bytecode, accountData.privateKey)
-                .then(async tokenReceipt => {
-                  // console.log(tokenReceipt, "here 2")
-                  projectData.tokenContractAddress = tokenReceipt.contractAddress;
-                  projectData.tokenContractHash = tokenReceipt.transactionHash;
-                  var IERC20 = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/IERC20.sol');
-                  var SafeERC20 = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/SafeERC20.sol');
-                  var SafeMath = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/SafeMath.sol');
-                  ejs.renderFile(__dirname + '/../contractCreator/ERC20contracts/Crowdsale.sol', {
-                    "SafeERC20": SafeERC20,
-                    "SafeMath": SafeMath,
-                    "IERC20": IERC20,
-                  }, async (err, data) => {
-                    nodemailerservice.sendContractEmail(req.user.email, data, req.body.coinName, "Crowdsale Contract");
-                    byteCode2 = await solc.compile(data, 1).contracts[':Crowdsale'];
-                    byteCode2.bytecode += web3.eth.abi.encodeParameters(['uint256', 'uint256', 'address', 'address', 'bool'], [projectData.ETHRate, projectData.bonusRate, '0x14649976AEB09419343A54ea130b6a21Ec337772', tokenReceipt.contractAddress, projectData.bonusStatus]).slice(2)
-                    projectData.crowdsaleByteCode = byteCode2.bytecode;
-                    projectData.crowdsaleABICode = byteCode2.interface;
-                    projectData.crowdsaleContractCode = data;
-                    privateICOhandler.sendTransaction(accountData.address, byteCode2.bytecode, accountData.privateKey)
-                      .then(async crowdsaleReceipt => {
-                        console.log(crowdsaleReceipt, "here 3")
-                        projectData.crowdsaleContractHash = crowdsaleReceipt.transactionHash;
-                        projectData.crowdsaleContractAddress = crowdsaleReceipt.contractAddress;
-                        await projectData.save();
-                        res.status(200).send({ tokenReceipt: tokenReceipt, crowdsaleReceipt: crowdsaleReceipt })
-                      })
-                      .catch(async e => {
-                        console.error('error in 2st deployment', e)
-                        projectData.crowdsaleContractAddress = "Network error occured! Please try again";
-                        projectData.tokenContractAddress = "Network error occured! Please try again";
-                        await projectData.save();
-                        res.status(200).send({ status: false, message: "Network error occured! Please try again" })
-
-                      })
-                  })
-                })
-                .catch(e => res.status(400).send({ status: false, message: "Network error occured! Please try again" })
-                );
+              contractDeployerListener.createAutomaticDeployer(req,projectData,accountData);
+              res.send({status:true,message:"Deployment is in process"})
             })
             .catch(e => res.status(400).send({ status: false, message: "Network error occured! Please try again" })
             );
@@ -121,97 +81,20 @@ module.exports = {
         try {
           etherRopstenICOhandler.sendEther(accountData.address, '0x06f05b59d3b20000')
             .then(async r => {
-              res.send({"status":"working"});
-              console.log("Step 2 deployment starts here");
-              byteCode = await solc.compile(projectData.tokenContractCode, 1).contracts[':Coin']
-              projectData.tokenByteCode = byteCode.bytecode;
-              projectData.tokenABICode = byteCode.interface;
-              etherRopstenICOhandler.sendTransaction(accountData.address, byteCode.bytecode, accountData.privateKey)
-                .then(async tokenReceipt => {
-                  console.log("Here:Ropsten Send Token reciept:",tokenReceipt)
-                  projectData.tokenContractAddress = tokenReceipt.contractAddress;
-                  projectData.tokenContractHash = tokenReceipt.transactionHash;
-                  var IERC20 = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/IERC20.sol');
-                  var SafeERC20 = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/SafeERC20.sol');
-                  var SafeMath = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/SafeMath.sol');
-                  ejs.renderFile(__dirname + '/../contractCreator/ERC20contracts/Crowdsale.sol', {
-                    "SafeERC20": SafeERC20,
-                    "SafeMath": SafeMath,
-                    "IERC20": IERC20,
-                  }, async (err, data) => {
-                    nodemailerservice.sendContractEmail(req.user.email, data, req.body.coinName, "Crowdsale Contract");
-                    byteCode2 = await solc.compile(data, 1).contracts[':Crowdsale'];
-                    byteCode2.bytecode += web3.eth.abi.encodeParameters(['uint256', 'uint256', 'address', 'address', 'bool'], [projectData.ETHRate, projectData.bonusRate, '0x14649976AEB09419343A54ea130b6a21Ec337772', tokenReceipt.contractAddress, projectData.bonusStatus]).slice(2)
-                    projectData.crowdsaleByteCode = byteCode2.bytecode;
-                    projectData.crowdsaleABICode = byteCode2.interface;
-                    projectData.crowdsaleContractCode = data;
-                    etherRopstenICOhandler.sendTransaction(accountData.address, byteCode2.bytecode, accountData.privateKey)
-                      .then(async crowdsaleReceipt => {
-                        console.log("Here:Ropsten Send Crowdsale reciept:",crowdsaleReceipt)
-                        projectData.crowdsaleContractHash = crowdsaleReceipt.transactionHash;
-                        projectData.crowdsaleContractAddress = crowdsaleReceipt.contractAddress;
-                        await projectData.save();
-                        res.status(200).send({ tokenReceipt: tokenReceipt, crowdsaleReceipt: crowdsaleReceipt })
-                      })
-                      .catch(async e => {
-                        console.error('error in 2st deployment', e)
-                        projectData.crowdsaleContractAddress = "Network error occured! Please try again";
-                        projectData.tokenContractAddress = "Network error occured!  Please try again";
-                        await projectData.save();
-                        res.status(400).send({ status: false, message: "Network error occured! Please try again" })
-                      })
-                  })
-                })
-                .catch(e => res.status(400).send({ status: false, message: "Network error occured! Please try again" })
-                );
+              contractDeployerListener.createAutomaticDeployer(req,projectData,accountData);
+              res.send({status:true,message:"Deployment is in process"})
+              // res.send({"status":"working"});
             })
             .catch(e => res.status(400).send({ status: false, message: "Network error occured! Please try again" })
             );
         } catch (e) {
+          contractDeployerListener.createAutomaticDeployer(req,projectData,accountData);
           res.status(400).send({ status: false, message: "Network error occured! Please try again" })
         }
       }
       else {
         try {
-          byteCode = await solc.compile(projectData.tokenContractCode, 1).contracts[':Coin']
-          projectData.tokenByteCode = byteCode.bytecode;
-          projectData.tokenABICode = byteCode.interface;
-          etherRopstenICOhandler.sendTransaction(accountData.address, byteCode.bytecode, accountData.privateKey)
-            .then(async tokenReceipt => {
-              projectData.tokenContractAddress = tokenReceipt.contractAddress;
-              projectData.tokenContractHash = tokenReceipt.transactionHash;
-              var IERC20 = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/IERC20.sol');
-              var SafeERC20 = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/SafeERC20.sol');
-              var SafeMath = await fileReader.readEjsFile(__dirname + '/../contractCreator/ERC20contracts/SafeMath.sol');
-              ejs.renderFile(__dirname + '/../contractCreator/ERC20contracts/Crowdsale.sol', {
-                "SafeERC20": SafeERC20,
-                "SafeMath": SafeMath,
-                "IERC20": IERC20,
-              }, async (err, data) => {
-                nodemailerservice.sendContractEmail(req.user.email, data, req.body.coinName, "Crowdsale Contract");
-                byteCode2 = await solc.compile(data, 1).contracts[':Crowdsale'];
-                byteCode2.bytecode += web3.eth.abi.encodeParameters(['uint256', 'uint256', 'address', 'address', 'bool'], [projectData.ETHRate, projectData.bonusRate, '0x14649976AEB09419343A54ea130b6a21Ec337772', tokenReceipt.contractAddress, projectData.bonusStatus]).slice(2)
-                projectData.crowdsaleByteCode = byteCode2.bytecode;
-                projectData.crowdsaleABICode = byteCode2.interface;
-                projectData.crowdsaleContractCode = data;
-                etherRopstenICOhandler.sendTransaction(accountData.address, byteCode2.bytecode, accountData.privateKey)
-                  .then(async crowdsaleReceipt => {
-                    projectData.crowdsaleContractHash = crowdsaleReceipt.transactionHash;
-                    projectData.crowdsaleContractAddress = crowdsaleReceipt.contractAddress;
-                    await projectData.save();
-                    res.status(200).send({ tokenReceipt: tokenReceipt, crowdsaleReceipt: crowdsaleReceipt })
-                  })
-                  .catch(async e => {
-                    console.error('error in 2st deployment', e)
-                    projectData.crowdsaleContractAddress = "Network error occured! Please try again";
-                    projectData.tokenContractAddress = "Network error occured! Please try again";
-                    await projectData.save();
-                    res.status(400).send({ status: false, message: "Network error occured! Please try again" })
-                  })
-              })
-            })
-            .catch(e => res.status(400).send({ status: false, message: "Network error occured! Please try again" })
-            );
+          
         } catch (e) {
           res.status(400).send({ status: false, message: "Network error occured! Please try again" })
         }
